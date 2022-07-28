7600a1aa0d3e980d984c0832209105727150f834
tls2123 <tlsgml2123@naver.com>
2022-07-28 15:29:31 +0900

    start

---
+++ front/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+//component
+import AppLayout from '../components/AppLayout';
+
+const Home = () => {
+    return(
+        <AppLayout>
+            <div>hello</div>
+        </AppLayout>
+    )
+}
+
+export default Home;
---
+++ front/pages/profile.js
@@ -0,0 +1,16 @@
+//프로필
+import React from "react";
+
+//component
+import AppLayout from "../components/AppLayout";
+
+const Profile = () => {
+    return(
+            <AppLayout>
+                <div>profile</div>
+            </AppLayout>
+
+    )
+}
+
+export default Profile;
